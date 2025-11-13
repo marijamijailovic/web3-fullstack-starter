ea1d71d8a3902cdb8da424d962c4031478d74a2a
Melnik George <melnik.goshan2013@gmail.com>
2025-11-13 20:39:11 +0300

    Added wagmi mock

--- packages/site/src/wagmi.config.ts
+++ packages/site/src/wagmi.config.ts
@@ -1,17 +1,17 @@
 import { createConfig, http } from "wagmi";
 import { lineaSepolia, hardhat } from "wagmi/chains";
-import { metaMask } from "wagmi/connectors";
+import { mock } from "wagmi/connectors";
 
 export const config = createConfig({
   multiInjectedProviderDiscovery: false,
   chains: [hardhat, lineaSepolia],
   connectors: [
-    metaMask({
-      dappMetadata: {
-        name: "Wagmi",
-        url: "https://wagmi.io",
-        iconUrl: "https://wagmi.io/favicon.ico",
-      },
+    mock({
+      accounts: [
+        '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+        '0x70997970c51812dc3a010c7d01b50e0d17dc79c8',
+        '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC',
+      ],
     }),
   ],
   syncConnectedChain: true,
